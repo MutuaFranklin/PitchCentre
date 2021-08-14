b962714cec409b10f9aa03e186dacaf05ebbf2bd
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-14 17:33:16 +0300

    user table update

--- app/models.py
+++ app/models.py
@@ -8,6 +8,37 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comment = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        print(self.password_secure)
+        return check_password_hash(self.password_secure,password)
+
+        
+    def __repr__(self):
+        return f'User {self.username}'
+
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     pitch_id = db.Column(db.Integer,primary_key = True)
@@ -60,36 +91,7 @@ class Comment(db.Model):
 
 
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
-    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
-    comment = db.relationship('Comment',backref = 'user',lazy = "dynamic")
-
-    
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_secure = generate_password_hash(password)
-
-
-    def verify_password(self,password):
-        print(self.password_secure)
-        return check_password_hash(self.password_secure,password)
-
-        
-    def __repr__(self):
-        return f'User {self.username}'
 
 
 class Role(db.Model):
