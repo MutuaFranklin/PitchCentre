ca582132537217bf19b19f19eb149f76a8955147
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-16 15:15:17 +0300

    Db formatting

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='1#237dge483'
 
 
-export MAIL_USERNAME='mbogiyamoringa@gmail.com'
-export MAIL_PASSWORD='Tryhackingm3bitch!'
+export MAIL_USERNAME='frankmailautomation@gmail.com'
+export MAIL_PASSWORD='Frank500#'
 
 python3 manage.py server
