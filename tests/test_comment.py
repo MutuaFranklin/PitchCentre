322c028a2a9dc8bbd373e888aba8e0eac26e43e4
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-17 11:30:09 +0300

    Testing

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,39 +1,34 @@
 
-# import unittest
-# from app.models import User, Pitch, Comment
-# from flask_login import current_user
-# from app import db
+import unittest
+from app.models import User, Comment
+from flask_login import current_user
+from app import db
 
-# class TestPitch(unittest.TestCase):
+class TestComment(unittest.TestCase):
 
-#     def setUp(self):
-#         self.user_frank = User(id = 2, username = 'frank',password = '54321', email = 'frankmutua@yahoo.com', bio = 'maniac',profile_pic_path = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png', role = 'Admin')
-#         self.new_pitch = Pitch(pitch_id=2, pitch_title='Data fetching with API',category = 'Technology', pitch_content ='This is how it goes', posted = '15/8/2021', user_id = 2, down_vote = 0, up_vote =0 )
-#         self.new_comment = Comment(id = 1, pitch_id =2, pitch_comment='dope stuff',posted='12/6/2021',user_id =1)
-
-
-#     def tearDown(self):
-#         Comment.query.delete()
-#         Pitch.query.delete()
+    def setUp(self):
+        self.user_frank = User(id = 1, username = 'frank', email = 'frankmutua@yahoo.com', password_secure = '54321')
+        self.new_comment = Comment(id = 1, pitch_id =2, pitch_comment='dope stuff',posted='12/6/2021',user_id =1)
 
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_comment,Comment))
+    def tearDown(self):
+        Comment.query.delete()
+        User.query.delete()
 
 
-#     def test_check_instance_variables(self):
-#         self.assertEquals(self.new_comment.id,1)
-#         self.assertEquals(self.new_comment.pitch_id,2)
-#         self.assertEquals(self.new_comment.pitch_comment,'dope stuff')
-#         self.assertEquals(self.new_comment.posted,'12/6/2021')
-#         self.assertEquals(self.new_comment.user_id, 1)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment,Comment))
 
 
-
-
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_comment.id,1)
+        self.assertEquals(self.new_comment.pitch_id,2)
+        self.assertEquals(self.new_comment.pitch_comment,'dope stuff')
+        self.assertEquals(self.new_comment.posted,'12/6/2021')
+        self.assertEquals(self.new_comment.user_id, 1)
 
 
   
-#     def test_save_comment(self):
-#         self.new_pitch.save_pitch()()
-#         self.assertTrue(len(Comment.query.all())>0)
+    # def test_save_comment(self):
+    #     self.new_comment.save_comment()
+    #     self.assertTrue(len(Comment.query.all())>0)
--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -1,23 +1,23 @@
 import unittest
-from app.models import User, Pitch
+from app.models import Comment, User, Pitch
 from flask_login import current_user
 from app import db
 
 class TestPitch(unittest.TestCase):
 
     def setUp(self):
-        self.user_frank = User(id = 29, username = 'frank',password = '54321', email = 'frankmutua@yahoo.com', bio = 'maniac',profile_pic_path = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png', role = 'Admin')
-        self.new_pitch = Pitch(pitch_id=34, pitch_title='Data fetching with API',category = 'Technology Pitch', pitch_content ='This is how it goes', posted = '15/8/2021', user_id = 29, down_vote = 0, up_vote =0 )
+        self.user_frank = User(id = 1, username = 'frank', email = 'frankmutua@yahoo.com', password_secure = '54321')
+        self.new_pitch = Pitch(pitch_id=34, pitch_title='Data fetching with API',category = 'Technology Pitch', pitch_content ='This is how it goes', posted = '15/8/2021', user_id = 1, up_vote =30, down_vote = 9,)
 
 
 
     def tearDown(self):
         Pitch.query.delete()
         User.query.delete()
-        db.session.delete(self.new_pitch)
-        db.session.commit()
-        db.session.delete(self.new_pitch)
-        db.session.commit()
+        # db.session.delete(self.new_pitch)
+        # db.session.commit()
+        # db.session.delete(self.new_pitch)
+        # db.session.commit()
 
 
     def test_instance(self):
@@ -27,18 +27,18 @@ class TestPitch(unittest.TestCase):
     def test_check_instance_variables(self):
         self.assertEquals(self.new_pitch.pitch_id,34)
         self.assertEquals(self.new_pitch.pitch_title,'Data fetching with API')
-        self.assertEquals(self.new_pitch.category,'Technology')
+        self.assertEquals(self.new_pitch.category,'Technology Pitch')
         self.assertEquals(self.new_pitch.pitch_content,'This is how it goes')
         self.assertEquals(self.new_pitch.posted,'15/8/2021')
-        self.assertEquals(self.new_pitch.user_id, 29)
-        self.assertEquals(self.new_pitch.up_vote,0)
-        self.assertEquals(self.new_pitch.down_vote,0)
+        self.assertEquals(self.new_pitch.user_id, 1)
+        self.assertEquals(self.new_pitch.up_vote,30)
+        self.assertEquals(self.new_pitch.down_vote,9)
 
 
 
 
 
   
-    def test_save_pitch(self):
-        self.new_pitch.save_pitch()
-        self.assertTrue(len(Pitch.query.all())>0)
+    # def test_save_pitch(self):
+    #     self.new_pitch.save_pitch()
+    #     self.assertTrue(len(Pitch.query.all())>0)
